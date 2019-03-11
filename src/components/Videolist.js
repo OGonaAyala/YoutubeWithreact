0c9f1300ecc2948a4f0d273ec1d6d76a8227d440
OGaonaAyala <gaonaayalaomar@gmail.com>
2019-03-11 16:36:23 -0600

    Resalta el video seleccionado

--- src/components/Videolist.js
+++ src/components/Videolist.js
@@ -1,28 +1,60 @@
 import React, {Component} from 'react';
 import VideoListItem from './video_list_item';
-//import { ListGroup, ListGroupItem } from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 class Videolist extends Component {
     constructor() {
         super();
-        this.state = { toggle: false };
+        this.state = {
+            toggle: false, 
+            value:0
+        };
         this.toggleButton = this.toggleButton.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
     }
     
     toggleButton() {
         this.setState(prevState => ({ toggle: !prevState.toggle }));
+        
+    }
+
+    handleSelect(value){
+        this.setState({
+            value: value   
+		});
     }
       
     render(){
         const toggle = this.state.toggle;
+        const style = {
+            background: '#FDC830',
+          };
         const videoItems = this.props.videos.map((video,index) => {
-        return (
-                <VideoListItem 
-                    userSelected={this.props.onSelect}           
-                    key={video.etag} 
-                    video={video}
-                    vista={this.state.toggle} />
+        if(this.state.value === index){
+            return (
+                <div key={index}>
+                    <VideoListItem 
+                        comparacion={this.handleSelect}
+                        userSelected={this.props.onSelect}           
+                        video={video}
+                        vista={this.state.toggle}
+                        indice={index}
+                        style={style} 
+                         />
+                </div>
+        );
+        }else{
+            return (
+                    <VideoListItem
+                        comparacion={this.handleSelect} 
+                        userSelected={this.props.onSelect}           
+                        key={video.etag} 
+                        video={video}
+                        vista={this.state.toggle}
+                        indice={index} 
+                        />
             );
+        }
         });
 
         return (
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -3,18 +3,41 @@ import React, {Component} from 'react';
 class VideoListItem extends Component {
     constructor(props){
         super(props);
-        this.state = { toggle: false };
-   
-      } 
+        this.state = { 
+            toggle: false, 
+            value: ' '
+        };
+        this.handleSelect = this.handleSelect.bind(this);
+      }
+      componentDidMount(){
+		this.setState({
+			value: this.props.indice
+        }) 
+    }
+
+    componentDidUpdate(prevProps) {
+        // Typical usage (don't forget to compare props):
+        if (this.props.indice !== prevProps.indice) {
+            this.setState({
+               value: this.props.indice
+            })
+            
+        }
+      }
+    
+    handleSelect(){
+        this.props.userSelected(this.props.video)
+        this.props.comparacion(this.state.value)
+    }
+      
     render(){
-    const video = this.props.video;    
+    const video = this.props.video;
     const imageUrl = video.snippet.thumbnails.default.url;
    
     if(this.props.vista === false){
         return (
-            <li onClick={() => this.props.userSelected(video)} className="list-group-item">
+            <li style={this.props.style} onClick={this.handleSelect} className="list-group-item">
                <div className="video-list media">
-    
                     <div className="media-left">
                         <img className="media-object" src={imageUrl} />
                     </div>
@@ -26,7 +49,7 @@ class VideoListItem extends Component {
         );
     }else{
         return (
-            <li onClick={() => this.props.userSelected(video)} className="list-group-item">
+            <li style={this.props.style}  onClick={this.handleSelect} className="list-group-item">
                <div className="video-list media">
                     <div className="media-body">
                         <div className="media-heading">{video.snippet.title}</div>
